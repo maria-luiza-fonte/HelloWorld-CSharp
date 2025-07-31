3b58e74a1893518cccabc890bbd9cb0d3a242a1b
maria-luiza-fonte <marialuizafonte946@gmail.com>
2025-07-31 10:33:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Bem vindo ao Mundo de Programação C#");
+        Console.WriteLine("Ola, Malu");
+
+        Console.WriteLine("A soma é igual a: " + soma);
+    }
+}
